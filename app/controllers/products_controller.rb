a6d167f049b6367bfcf7fc0bd2703f918e8eb142
fullgrown31 <dwightmopas@ymail.com>
2019-02-05 05:06:10 -0600

    Finished

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,8 @@
 class ProductsController < ApplicationController
   def index
     @products = Product.all
+    @products_total = Product.count
+    @products_average = Product.average(:price)
+    @products_stock_total = Product.sum(:stock_quantity)
   end
 end
